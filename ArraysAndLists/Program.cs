6bc72d4b7687d7c3e01463bbb4d9dea04084b98f
Rachel Hodge <rchodge9@gmail.com>
2021-03-15 23:18:48 -0500

    finished

--- ArraysAndLists/Program.cs
+++ ArraysAndLists/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ArraysAndLists
 {
@@ -6,27 +7,52 @@ namespace ArraysAndLists
     {
         static void Main(string[] args)
         {
-            //TODO:
+            //DONE
 
             // Create an int Array and populate numbers 1-10
+            //explicit typing
+            int[] numbers = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            // inferred typing
+            var charArray = new char[] { '1', '2', '3', '4', '5', '6', '7', '8', '9','0' };
 
             /* Create two Lists of type int.
              * Name one List "evens"
              * Name the other List "odds"
              */
-
+            var evens = new List<int>();
+            var odds = new List<int>();
             /* Using either a foreach or for loop,
              * nest an if statement to check to see
              *  if a number is even or odd.
              * Then add those numbers to either the evens List
              * or the odds List
              */
-
+            foreach (var num in numbers)
+            {
+                 if(num % 2==0)
+                {
+                    evens.Add(num);
+                }
+                 else
+                {
+                    odds.Add(num);
+                }
+            }
+            
             /* Now using foeach or for loops,
              * display each List of even and odd numbers
              *
              * Try to be creative in your display
              */
+            foreach (var x in evens)
+            {
+                Console.WriteLine($"{x} is an even number");
+            }
+            foreach (var y in odds)
+            {
+                Console.WriteLine($"{y} is an odd number");
+            }
         }
     }
 }
